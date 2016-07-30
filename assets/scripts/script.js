ee07e8c885fda2ff6ce8dcdaa0471f4e7ffc09d3
James Zhang <james.zhang75024@gmail.com>
2016-07-29 21:19:32 -0500

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,25 @@
+console.log('hello world');
+
+/* set up XMLHttpRequest */
+var url = "data/xlxs_file_for_test.xlsx";
+var oReq = new XMLHttpRequest();
+oReq.open("GET", url, true);
+oReq.responseType = "arraybuffer";
+
+oReq.onload = function(e) {
+  var arraybuffer = oReq.response;
+
+  /* convert data to binary string */
+  var data = new Uint8Array(arraybuffer);
+  var arr = new Array();
+  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+  var bstr = arr.join("");
+
+  /* Call XLSX */
+  var workbook = XLSX.read(bstr, {type:"binary"});
+
+  /* DO SOMETHING WITH workbook HERE */
+  console.log(workbook);
+}
+
+oReq.send();
